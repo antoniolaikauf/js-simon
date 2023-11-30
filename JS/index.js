95539e222f1b3916f635020fa9faf3d544e8f030
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-30 17:51:39 +0100

    feat: aggiunto contatore di numeri sbagliati e giusti

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,11 @@
+
 // creazione della variabile dove si vedranno gli elementi
 const arrayNumRandom= numerGenerated(1,100,5);
 console.log(arrayNumRandom);
 
 
 // inserimeto numeri dentro html cosi utene può vedere
-const display= document.getElementById("js-numeri");
+const display= document.getElementById("display-numeri");
 display.innerHTML=arrayNumRandom;
 
 // variabile dove c'è il display della rispasta
@@ -15,7 +16,7 @@ displayRisposta.style.display="none"
 setTimeout(() => {
     display.style.display="none"
     displayRisposta.style.display="block"
-}, 3000);
+}, 300);
 
 // variabile dove vengono inseriti i numeri da parte dell'utente
 const numeriInseriti= document.getElementById("inserimento-numeri");
@@ -28,19 +29,29 @@ const arrayNumSbagliati= [];
 
 const arrayNumGiusti= [];
 
+let wrong=0;
+let right=0;
+
 // variabile con bottone per evento
     
 let button=document.getElementById("js-controllo");
 button.addEventListener("click",function(){
     let valore=parseInt(numeriInseriti.value); 
 
-    smistamentoValori(valore);
-
-    // quando tentativi finiti mettere i risultati dentro html ( messo arrayrandom.length cosi che se cambia la quantità dei numeri random si avra i stessi tentativi quanti sono i numeri random)
-    if (arrayNumGiusti.length + arrayNumSbagliati.length === arrayNumRandom.length) {
-
-        outputUtente.innerHTML = "hai sbagliato questi numeri " + arrayNumSbagliati + "   hai fatto giusto questi numeri " + arrayNumGiusti
+    if (arrayNumRandom.includes(valore)) {
+          arrayNumGiusti.push(valore)
+          right++
+        // console.log(arrayNumGiusti);
+    }else{
+         arrayNumSbagliati.push(valore)
+         wrong++
+        // console.log(arrayNumSbagliati);
     }
+
+    if (arrayNumGiusti.length + arrayNumSbagliati.length === arrayNumRandom.length) {
+        outputUtente.innerHTML = " sbagliate " + wrong+ " hai sbagliato questi numeri " + arrayNumSbagliati +   " giuste " + right +  " hai fatto giusto questi numeri " + arrayNumGiusti
+    }
+    
 });
 
 
@@ -66,17 +77,3 @@ function randomNumber(max, min) {
     return Math.floor(Math.random() * max + min)
 };
 
-
-// funzione per controllare valori
-function smistamentoValori(numScelto) {
-        // controllo se ha fatto giusto o no
-        if (arrayNumRandom.includes(numScelto)) {
-            outputUtente.innerHTML = numScelto + "è corretto"
-             return arrayNumGiusti.push(numScelto)
-            // console.log(arrayNumGiusti);
-        }else{
-            outputUtente.innerHTML = numScelto + " è sbagliato"
-           return  arrayNumSbagliati.push(numScelto)
-            // console.log(arrayNumSbagliati);
-        }
-}
